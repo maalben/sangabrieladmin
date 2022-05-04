bc42d60507a71437c1c89d70e4fdb2a6c1ad522c
Mario Alejandro Benitez Orozco <maalben@gmail.com>
2022-05-04 16:50:28 -0500

    Se suben ajustes de la seguridad

--- connection/Connection.php
+++ connection/Connection.php
@@ -2,13 +2,21 @@
 
 class Connection{
 
+    private static $urlBase;
+
 //    public static function getConnection(){
+//        self::$urlBase = 'sangabrieladmin/';
 //        $connection = new mysqli("127.0.0.1", "root", "12345678", "bdsangabriel");
 //        $connection->query("SET NAMES 'utf8'");
 //        return $connection;
 //    }
 
+    public static function getUrlByPermission(){
+        return self::$urlBase;
+    }
+
     public static function getConnection(){
+        self::$urlBase = 'adminsangabriel/';
         $connection = new mysqli("localhost", "funerari_funeraR", "r5,gAos0V[}n", "funerari_BASEWP");
         $connection->query("SET NAMES 'utf8'");
         return $connection;
